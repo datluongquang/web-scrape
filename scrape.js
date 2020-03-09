a35c9b4948f8cfd552cd1ee7840b0839f7e07535
datluongquang <35949060+datluongquang@users.noreply.github.com>
2020-03-09 16:07:30 -0400

    someshit

--- scrape.js
+++ scrape.js
@@ -16,14 +16,22 @@ const getResults = async (url) => {
     // $("td[headers='view-field-course-number-cs-table-column']").each((index, element) => {
     //     tags.add($(element).text().replace(/\s/g,''));
     // });
+    let count=0;
     $("table tbody tr").each((index, element) => {
         const bodyTr= $(element).children("td");
         //console.log(bodyTr)
         const dummy= [];
         Array.prototype.forEach.call(bodyTr, child => {
-            dummy.push($(child).text().replace(/\s/g,''));
+            dummy.push($(child).text().trim());
         });
-        console.log(dummy)
+        count=dummy.length;
+        if(count==13){
+            dummy.splice(5, 0, "");
+        }
+        else if( count==12){
+            dummy.splice(4,0,"");
+            dummy.splice(5,0,"");
+        }
         tags.add(dummy)
     });
     const next= $(".pager__link.pager__link--next").attr('href');
